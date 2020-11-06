648f6793bbbad35bba3d138d8292cfb772957a3a
Webdev-DavidM <footballblubber@googlemail.com>
2020-11-06 15:28:02 +0000

    Updates

---
+++ precache-manifest.a5d238ba71eb1b5c9c75bf5a4b7d220e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f78cfdde4acabefc190f5761be64d4e9",
+    "url": "/udacity-react-myreads/index.html"
+  },
+  {
+    "revision": "ed002ab09906d3ee752f",
+    "url": "/udacity-react-myreads/static/css/main.fc523a19.chunk.css"
+  },
+  {
+    "revision": "3ef7aef1d95b9026e567",
+    "url": "/udacity-react-myreads/static/js/2.385ab0c2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/udacity-react-myreads/static/js/2.385ab0c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed002ab09906d3ee752f",
+    "url": "/udacity-react-myreads/static/js/main.20393be5.chunk.js"
+  },
+  {
+    "revision": "ef1fecb188acdd2304ff",
+    "url": "/udacity-react-myreads/static/js/runtime-main.f3f5b343.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/udacity-react-myreads/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/udacity-react-myreads/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/udacity-react-myreads/static/media/arrow-drop-down.78c6f53b.svg"
+  }
+]);
